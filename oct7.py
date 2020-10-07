e3eb3e619a459eb9ca22b326313d1f1683038a6a
Niqole-zhao <70828769+Niqole-zhao@users.noreply.github.com>
2020-10-07 20:54:49 +0800

    Add files via upload

---
+++ oct7.py
@@ -0,0 +1,38 @@
+
+# import turtle
+# turtle.setup()   #新建窗口，进行绘画
+# turtle.left(45)
+# turtle.fd(100)
+# turtle.left(45)
+# turtle.fd(100)
+# turtle.setheading(135)
+# turtle.fd(100)
+# turtle.left(45)
+# turtle.fd(100)
+# turtle.left(45)
+# turtle.fd(100)
+# turtle.left(45)
+# turtle.fd(100)
+# turtle.left(45)
+# turtle.fd(100)
+# turtle.left(45)
+# turtle.fd(100)
+# turtle.done()  #放在最后，否则窗口会消失
+
+"""剪刀石头布的游戏"""
+# import random
+# a=["石头","剪刀","布"]
+# b=input("请从剪刀、石头、布里面选择你要出的手势：")
+# c=random.choice(a)
+# print("机器选择了：",c)
+# if b==c:
+#     print('平局')
+# elif b=="石头" and c=="剪刀":
+#     print("你赢了")
+# elif b=="剪刀" and c=="布":
+#     print("你赢了")
+# elif b == "布" and c == "石头":
+#     print("你赢了")
+# else:
+#     print("你输了")
+
